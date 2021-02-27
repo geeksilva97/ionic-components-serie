7617c5a5cb300d8a6660b45dd2b33a285b024aa8
Edigleysson Silva <edigleyssonsilva@gmail.com>
2021-02-26 22:29:16 -0300

    feat: modal demo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ModalPage } from '../modal/modal.page';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private modalCtrl: ModalController
+  ) {}
+
+
+  async showModal() {
+    console.log('showModal()');
+    const modal = await this.modalCtrl.create({
+      component: ModalPage,
+      cssClass: 'my-custom-modal-css',
+      componentProps: {
+        name: 'Edy Silva',
+        email: 'edigleyssonsilva@codesilva.github.io'
+      }
+    });
+
+    // will present
+
+    await modal.present();
+
+    // didpresent
+
+    // desestruturação
+    //
+    const { data } = await modal.onDidDismiss();
+    console.log({data});
+  }
 
 }
